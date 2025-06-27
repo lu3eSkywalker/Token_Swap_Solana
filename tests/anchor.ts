6f42ff69335978d31bea19562906a93d36464895
lu3eskywalker <surajjhajhria1@gmail.com>
2025-06-27 22:14:30 +0530

    adding test case for token swapping for a userKeypair

--- tests/anchor.ts
+++ tests/anchor.ts
@@ -3,10 +3,23 @@ import * as web3 from "@solana/web3.js";
 import { CpiGuardLayout, getAccount, getAssociatedTokenAddress } from "@solana/spl-token";
 import { SimpleTokenSwap } from "../target/types/Simple_Token_Swap";
 import { BN } from "bn.js";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+
+const base58PrivateKey = "";
+const privateKeySeed = bs58.decode(base58PrivateKey);
+
+const userKeyPair = web3.Keypair.fromSecretKey(privateKeySeed);
+
+const connection = new web3.Connection("https://api.devnet.solana.com", "confirmed");
+const userWallet = new anchor.Wallet(userKeyPair);
+const provider = new anchor.AnchorProvider(connection, userWallet, {
+  preflightCommitment: "confirmed",
+});
+anchor.setProvider(provider);
 
 describe("Test", () => {
   // Configure the client to use the local cluster
-  anchor.setProvider(anchor.AnchorProvider.env());
+  // anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.SimpleTokenSwap as anchor.Program<SimpleTokenSwap>;
 
@@ -17,7 +30,7 @@ describe("Test", () => {
     const [vault_token_account, bump1] = await web3.PublicKey.findProgramAddressSync(
       [Buffer.from("vaultTokenA"), tokenA_mint_address.toBuffer()],
       program.programId
-    )
+    );
 
     const [vaultPDA, bump2] = await web3.PublicKey.findProgramAddressSync(
       [Buffer.from("vaultTokenA"), tokenA_mint_address.toBuffer()],
@@ -249,5 +262,63 @@ describe("Test", () => {
 
       // Confirm Transaction
       await program.provider.connection.confirmTransaction(txHash);
-  })
+  });
+
+    it("Swap Token A for Token B for a user", async () => {
+
+    const userATAforTokenA = new web3.PublicKey("2SMbR29233K31GsL5dzyXixHHTzqjEMHkBTEB7ngnQrZ");
+    const userATAforTokenB = new web3.PublicKey("Dp3oxBTFVjuf4uB5mb4xJQDLCaZQnNrc9Q2HxpLc8bki");
+
+    const userPublicKey = new web3.PublicKey("HVw1Z2KFYfKjdL2UThi5RGBvSUpsF4zdsPrucV8TggQm");
+
+    const [vault_token_account_a, bump1] = await web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("vaultTokenA"), tokenA_mint_address.toBuffer()],
+      program.programId
+    );
+
+    const [vault_token_account_b, bump2] = await web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("vaultTokenB"), tokenB_mint_address.toBuffer()],
+      program.programId
+    );
+
+    const [vault_auth_a, bump3] = await web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("vaultTokenA"), tokenA_mint_address.toBuffer()],
+      program.programId
+    );
+
+    const [vault_auth_b, bump4] = await web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("vaultTokenB"), tokenB_mint_address.toBuffer()],
+      program.programId
+    );
+
+    const vaultTokenAVault = await getAccount(program.provider.connection, vault_token_account_a);
+    const vaultTokenBVault = await getAccount(program.provider.connection, vault_token_account_b);
+
+    console.log("This is the value of vault_token_account_a: ", vaultTokenAVault.amount.toString());
+    console.log("This is the value of vault_token_account_b: ", vaultTokenBVault.amount.toString());
+
+    const amount = new BN(10_000_000_000);
+
+    const txHash = await program.methods
+      .swapAForB(amount)
+      .accounts({
+        user: userPublicKey,
+        userTokenAccountForTokenA: userATAforTokenA,
+        userTokenAccountForTokenB: userATAforTokenB,
+        vaultTokenAAccount: vault_token_account_a,
+        vaultTokenBAccount: vault_token_account_b,
+        vaultAuthA: vault_auth_a,
+        vaultAuthB: vault_auth_b,
+        mintA: tokenA_mint_address,
+        mintB: tokenB_mint_address,
+        tokenProgram: new web3.PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"),
+      })
+      .signers([userKeyPair])
+      .rpc();
+
+      console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
+
+      // Confirm Transaction
+      await program.provider.connection.confirmTransaction(txHash);
+  });
 });
